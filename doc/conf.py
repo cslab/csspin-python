cb1cd39b50b8f83751f3f9f53cab91aea3d43dc4
Benjamin Thomas Schwertfeger <benjaminthomas.schwertfeger@contact-software.com>
2024-10-22 08:46:51 +0000

    Resolve "python: Document the behavior how the environment variables come into the virtual environment"

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,13 @@ with Path("links.rst").open(encoding="utf-8") as f:
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "plugins/*_schemaref.rst"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "plugins/*_schemaref.rst",
+    "links.rst",
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
